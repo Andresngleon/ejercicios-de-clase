41c188757dfde7ca48b9b31ccc819009388743bb
Andresngleon <andresngleon@gmail.com>
2022-03-12 12:02:01 -0600

    Primera clase de JS

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//es una espacio de memoria donde se almacena la info de la applicacion
+//declaracion: nombre a un espacio de memoria
+let nombre;
+//asignacion: asigna valor a un espacio de mem
+nombre='Keylor';
+
+//tipo de dato: string, number, boolean, object, array
+//string: cadena de caracteres
+let apellido= 'Perez'
+//boolean: true o false
+let casado = 30
+//number: numeros
+let persona = {
+    nombre: 'juan',
+    apellido: 'perez',
+    edad: 30
+}
+//object: objds
+//array: listas de datos
+let languages = ['Javascript', 'PHP','Python', 2, true,{nombre:'Juan', apellido:'Perez'}]
+//tipos declaracion JS
+//const constante no se puede cambiar
+//var, variable global
+//let variable local porque depende de su contexto y esta dentro de una estructura de flujo determinada
