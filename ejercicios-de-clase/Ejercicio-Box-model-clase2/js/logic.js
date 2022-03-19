2a7fc34f85c51d90df01d1c47daf8a0087a6fd5b
Andresngleon <andresngleon@gmail.com>
2022-03-19 12:05:53 -0600

    Ejercicio 2
    Ejercicio 2

---
+++ ejercicios-de-clase/Ejercicio-Box-model-clase2/js/logic.js
@@ -0,0 +1,13 @@
+//traer elementos del DOM en Javascript
+console.log("LOGIC");
+var element = document.getElementById('image')
+console.log('elemento',element);
+
+var paragraphList = document.getElementsByClassName('p')
+console.log('getElementsByTagName',paragraphList);
+
+var paragraphListByClass = document.getElementsByClassName('paragraph')
+console.log('getElementsByTagName',paragraphList);
+
+//como agregar css a un elemento
+element.classList.add(title)
